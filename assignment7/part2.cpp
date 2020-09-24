a7fffa0b5641281735687b61e6a7b7dc30785773
sebastian <darealzantiki@gmail.com>
2020-09-24 15:16:21 +0200

    Some basic part2 stuff

--- assignment7/part2.cpp
+++ assignment7/part2.cpp
@@ -1,4 +1,28 @@
 //
 // Created by sebastian on 24.09.2020.
 //
+# include <vector>
+# include <string>
 
+class Set{
+public:
+    std::vector<int> set_vector;
+
+    Set(std::vector<int> init_vector){
+    // Create unique vector;
+    }
+    Set operatorU(const Set &other) const {
+        std::vector<int> result_vector;
+        return other;
+    }
+
+    std::string str() const{
+        //return std::string str(this->set_vector.begin(), this->set_vector.end());
+    }
+
+};
+
+int main(){
+    Set set1 = Set({1,2,3});
+    Set set2 = Set({4,5,6});
+}
